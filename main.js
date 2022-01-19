584ac4cbc8fd70c11d770aecaf8be7f9c1111ab5
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2022-01-19 13:57:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function gotPoses(results)
     console.log(results);
     noseX = results[0].pose.nose.x;
     noseY = results[0].pose.nose.y;
-    console.log("narizX = " + noseX +" narizY = " + noseY);
+    console.log("noseX = " + noseX +" noseY = " + noseY);
 
     leftWristX = results[0].pose.leftWrist.x;
     rightWristX = results[0].pose.rightWrist.x;
